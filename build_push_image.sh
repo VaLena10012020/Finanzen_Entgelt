f66136b181850bace65527bd5221d2be3642cdc3
Lena <mdeschner@hotmail.de>
2021-01-30 18:38:12 +0100

    add build push docker image script

---
+++ build_push_image.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e # exit immediately in case of error
+set -x # print all executed commands (=debug mode)
+
+
+echo "=== Get latest docker image ==="
+
+: ${REGISTRY=001328938962.dkr.ecr.us-east-2.amazonaws.com}
+: ${REPOSITORY=valena}
+
+# login to aws ecr
+aws --region us-east-2 ecr get-login-password | docker login --username AWS --password-stdin ${REGISTRY}
+
+# get latest docker image for caching if available
+docker pull ${REGISTRY}/${REPOSITORY}:main || true
+
+
+echo "=== Build new docker image ==="
+
+docker build --pull=true --cache-from ${REGISTRY}/${REPOSITORY}:main \
+  -t ${REGISTRY}/${REPOSITORY}:${TRAVIS_BRANCH} .
+
+# to do add test script for docker image
+
+echo "=== Push docker image ${TRAVIS_BRANCH} to AWS ECR ==="
+
+docker push ${REGISTRY}/${REPOSITORY}:${TRAVIS_BRANCH}
