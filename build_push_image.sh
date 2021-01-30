b0adde8d9fb4530ca6fd8ae8e1dce483d24f1d1b
Lena <mdeschner@hotmail.de>
2021-01-30 20:19:27 +0100

    pull ECR_REGISTRY and ECR_REPOSITORY value as env var from travis

--- build_push_image.sh
+++ build_push_image.sh
@@ -5,24 +5,20 @@ set -x # print all executed commands (=debug mode)
 
 
 echo "=== Get latest docker image ==="
-
-: ${REGISTRY=001328938962.dkr.ecr.us-east-2.amazonaws.com}
-: ${REPOSITORY=valena}
-
 # login to aws ecr
-docker login -u AWS -p $(aws --region us-east-2 ecr get-login-password) ${REGISTRY}
+docker login -u AWS -p $(aws --region us-east-2 ecr get-login-password) ${ECR_REGISTRY}
 
 # get latest docker image for caching if available
-docker pull ${REGISTRY}/${REPOSITORY}:main || true
+docker pull ${ECR_REGISTRY}/${ECR_REPOSITORY}:main || true
 
 
 echo "=== Build new docker image ==="
 
-docker build --pull=true --cache-from ${REGISTRY}/${REPOSITORY}:main \
-  -t ${REGISTRY}/${REPOSITORY}:${TRAVIS_BRANCH} .
+docker build --pull=true --cache-from ${ECR_REGISTRY}/${ECR_REPOSITORY}:main \
+  -t ${ECR_REGISTRY}/${ECR_REPOSITORY}:${TRAVIS_BRANCH} .
 
 # to do add test script for docker image
 
 echo "=== Push docker image ${TRAVIS_BRANCH} to AWS ECR ==="
 
-docker push ${REGISTRY}/${REPOSITORY}:${TRAVIS_BRANCH}
+docker push ${ECR_REGISTRY}/${ECR_REPOSITORY}:${TRAVIS_BRANCH}
