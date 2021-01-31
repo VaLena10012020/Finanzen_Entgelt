73b578fd8c3a5f21d170cd241bc1803b8dbef90e
ValentinKuhn <valentin.gabriel.kuhn@outlook.de>
2021-01-31 09:51:42 +0100

    Changed target to S3 and file to csv

--- app.py
+++ app.py
@@ -3,40 +3,44 @@ import os
 import sys
 
 from finanzen_base.Utils.s3connector import S3Connector
+from finanzen_base.Utils.extract_filename import extract_filename
 from EntgeltUtils.pdf_parser import PdfParser
 
 if __name__ == '__main__':
     # Set individuals for the app: filepath and APP_NAME
     filepath = os.getcwd()
     bucket_name = "valena1databucket"
+    bucket_source = "files/Entgelt/Audi"
+    bucket_target = "database/raw/Entgelt/Audi/"
     APP_NAME = 'Entgelt'
     params = sys.argv
 
     # Initialise PDF_Parser
     Parser = PdfParser()
 
-    # Initialise connection to database
-    # MongoConnection = MongoConnect(collect=APP_NAME)
-
     # Connect to s3 bucket
     con = S3Connector(bucket_name=bucket_name)
 
-    while con.connected:
-        # files_in_db = MongoConnection.get_distinct_item("file")
-        files_in_cloud = con.list_objects(bucket_name=bucket_name,
-                                          prefix="Entgelt/Audi")
-        list_pdfs = []
-        # for file in list(files_in_cloud.keys()):
-        #    if file not in files_in_db and ".pdf" in file:
-        #        list_pdfs.append(file)
+    while True:
+        files_raw = extract_filename(con.list_objects(bucket_name=bucket_name,
+                                                      prefix=bucket_source))
+        files_parsed = extract_filename(con.list_objects(bucket_name,
+                                                         bucket_target))
+        list_pdfs = {}
+        for file in files_raw:
+            if files_raw[file] not in files_parsed.values() and ".pdf" in file:
+                list_pdfs[file] = files_raw[file]
 
         if len(list_pdfs) > 0:
+            temp_file_name = "temp_file"
             for pdf in list_pdfs:
-                filename = pdf.split("/")[-1]
-                con.download_file(filepath=pdf, target_path=filename)
-                pdf_parsed_list = Parser.parse_entgelt(filename)
-                # MongoConnection.insert_dicts(pdf_parsed_list)
-                os.remove(filename)
+                con.download_file(filepath=pdf, target_path=list_pdfs[pdf])
+                pdf_parsed_df = Parser.parse_entgelt(list_pdfs[pdf])
+                pdf_parsed_df.to_csv(temp_file_name)
+                con.upload_file(file_path=temp_file_name,
+                                target_path=bucket_target + list_pdfs[pdf])
+                os.remove(list_pdfs[pdf])
+                os.remove(temp_file_name)
         else:
             print("No new pdfs found")
-        time.sleep(30*60)
+        time.sleep(30 * 60)
--- tests/EntgeltUtils/test_Pdf_Parser.py
+++ tests/EntgeltUtils/test_Pdf_Parser.py
@@ -1,4 +1,5 @@
 import pytest
+import pandas as pd
 from EntgeltUtils.pdf_parser import PdfParser
 
 
@@ -8,6 +9,6 @@ from EntgeltUtils.pdf_parser import PdfParser
 def test_single_and_multi_table(filename):
     parser = PdfParser()
     table_parsed_1 = parser.parse_entgelt(filename="./tests/data/" + filename)
-    assert type(table_parsed_1) is list
-    assert str(table_parsed_1[0]["date"]) == '2020-01-01 00:00:00'
-    assert table_parsed_1[0]['file'] == filename
+    assert type(table_parsed_1) is pd.DataFrame
+    assert str(table_parsed_1["date"][0]) == '2020-01-01 00:00:00'
+    assert table_parsed_1['file'][0] == filename
