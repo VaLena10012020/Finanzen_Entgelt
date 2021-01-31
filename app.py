4885f6712644549016eedcd63272e475747fd53c
ValentinKuhn <valentin.gabriel.kuhn@outlook.de>
2021-01-31 10:10:12 +0100

    Changed App to class

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import time
 import os
 import sys
 
@@ -6,41 +5,67 @@ from finanzen_base.Utils.s3connector import S3Connector
 from finanzen_base.Utils.extract_filename import extract_filename
 from EntgeltUtils.pdf_parser import PdfParser
 
-if __name__ == '__main__':
-    # Set individuals for the app: filepath and APP_NAME
-    filepath = os.getcwd()
-    bucket_name = "valena1databucket"
-    bucket_source = "files/Entgelt/Audi"
-    bucket_target = "database/raw/Entgelt/Audi/"
-    APP_NAME = 'Entgelt'
-    params = sys.argv
 
-    # Initialise PDF_Parser
-    Parser = PdfParser()
+class App:
+    def __init__(self, bucket_name: str = "valena1databucket",
+                 bucket_source: str = "files/Entgelt/Audi",
+                 bucket_target: str = "database/raw/Entgelt/Audi/",
+                 APP_NAME: str = 'Entgelt',
+                 params: str = None):
+        self.bucket_name = bucket_name
+        self.bucket_source = bucket_source
+        self.bucket_target = bucket_target
+        self.APP_NAME = APP_NAME
+        self.Parser = None
+        self.con = None
 
-    # Connect to s3 bucket
-    con = S3Connector(bucket_name=bucket_name)
+    def initialize(self):
+        self.Parser = PdfParser()
+        self.con = S3Connector(bucket_name=self.bucket_name)
 
-    while True:
-        files_raw = extract_filename(con.list_objects(bucket_name=bucket_name,
-                                                      prefix=bucket_source))
-        files_parsed = extract_filename(con.list_objects(bucket_name,
-                                                         bucket_target))
-        list_pdfs = {}
-        for file in files_raw:
-            if files_raw[file] not in files_parsed.values() and ".pdf" in file:
-                list_pdfs[file] = files_raw[file]
+    def check_for_unparsed_files(self):
+        f_raw = self.con.list_objects(bucket_name=self.bucket_name,
+                                      prefix=self.bucket_source)
+        f_raw = extract_filename(f_raw)
+        f_parsed = self.con.list_objects(bucket_name=self.bucket_name,
+                                         prefix=self.bucket_target)
+        f_parsed = extract_filename(f_parsed
+                                    )
+        self.f_unparsed = {}
+        for file in f_raw:
+            if f_raw[file] not in f_parsed.values() and ".pdf" in file:
+                self.f_unparsed[file] = f_raw[file]
 
-        if len(list_pdfs) > 0:
+    def parse_files(self):
+        if len(self.f_unparsed) > 0:
             temp_file_name = "temp_file"
-            for pdf in list_pdfs:
-                con.download_file(filepath=pdf, target_path=list_pdfs[pdf])
-                pdf_parsed_df = Parser.parse_entgelt(list_pdfs[pdf])
+            for pdf in self.f_unparsed:
+                self.con.download_file(filepath=pdf,
+                                       target_path=self.f_unparsed[pdf])
+                pdf_parsed_df = self.Parser.parse_entgelt(self.f_unparsed[pdf])
                 pdf_parsed_df.to_csv(temp_file_name)
-                con.upload_file(file_path=temp_file_name,
-                                target_path=bucket_target + list_pdfs[pdf])
-                os.remove(list_pdfs[pdf])
+
+                file_upload_name = self.bucket_target+self.f_unparsed[pdf][-4]+".csv"
+                self.con.upload_file(file_path=temp_file_name,
+                                     target_path=file_upload_name)
+                os.remove(self.f_unparsed[pdf])
                 os.remove(temp_file_name)
+            return self.f_unparsed
         else:
             print("No new pdfs found")
-        time.sleep(30 * 60)
+
+
+if __name__ == '__main__':
+    # Set individuals for the app: filepath and APP_NAME
+    params = sys.argv
+    APP = App(bucket_name="valena1databucket",
+              bucket_source="files/Entgelt/Audi",
+              bucket_target="database/raw/Entgelt/Audi/",
+              APP_NAME='Entgelt',
+              params=params)
+    # Initialise PDF_Parser
+    APP.initialize()
+
+    APP.check_for_unparsed_files()
+
+    APP.parse_files()
