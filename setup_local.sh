dac4a2ca2932af14bdafd5a1c1419d2a2f050744
ValentinKuhn <valentin.gabriel.kuhn@outlook.de>
2021-01-31 19:45:11 +0100

    Rebase main

--- setup_local.sh
+++ setup_local.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip install git+https://${GITHUB_TOKEN}:x-oauth-basic@github.com/VaLena10012020/Finanzen_python_base.git@minor_updates#egg=finanzen_base
+pip install git+https://${GITHUB_TOKEN}:x-oauth-basic@github.com/VaLena10012020/Finanzen_python_base.git@v0.1.0#egg=finanzen_base
 pip install -e .[dev]
 pre-commit install
 pre-commit autoupdate
