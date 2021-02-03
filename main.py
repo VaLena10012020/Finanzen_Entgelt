acf87b7d06d4ed8e4981bf5fbb23e32b6e1115c9
ValentinKuhn <valentin.gabriel.kuhn@outlook.de>
2021-02-03 22:07:49 +0100

    rename APP to app

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from EntgeltUtils.app import App
 
 if __name__ == '__main__':
     # Set individuals for the app: filepath and APP_NAME
-    APP = App(bucket_name="valena1databucket",
+    app = App(bucket_name="valena1databucket",
               bucket_source="files/Entgelt/Audi",
               bucket_target="database/raw/Entgelt/Audi/",
               name='Entgelt')
-    APP.check_for_unparsed_files()
-    APP.parse_files()
+    app.check_for_unparsed_files()
+    app.parse_files()
