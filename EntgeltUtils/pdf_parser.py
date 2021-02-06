1f7b71ffc1fd525b6a9a58c38c8ee8bae3327181
ValentinKuhn <valentin.gabriel.kuhn@outlook.de>
2021-02-06 18:09:38 +0100

    Set up target dict and parse schema

--- EntgeltUtils/pdf_parser.py
+++ EntgeltUtils/pdf_parser.py
@@ -29,13 +29,45 @@ class PdfParser:
         # rename columns
         df_out.columns = ["type", "1", "2", "3", "4", "value2", "value", "7"]
 
-        # Get date of Entgelt via row "Überweisung zum EndOfMonth"
-        date_raw = df_out["type"][df_out["type"].str.contains(
-            "Überweisung zum", regex=True, na=False)].to_list()[0]
-        df_out["date"] = datetime.strptime(date_raw[16:], "%d.%m.%Y")
+        # read schema definitions
+        schema_defintion = pd.read_csv("./config/entgelt_definition.csv")
+        df_dict = {}
 
-        # Set Name of File
-        df_out["file"] = filename.split("/")[-1]
+        # Get date of Entgelt via row "Überweisung zum EndOfMonth"
+        for i, schema in schema_defintion.itterrow():
+            parsed = self.parse_df_field(df=df_out,
+                                         search_column=schema["search_column"],
+                                         target_column=schema["target_column"],
+                                         search_str=schema["search_str"],
+                                         split=schema["split"],
+                                         output_float=schema["output_float"])
+            if schema['parser_type'] == 'date':
+                parsed = datetime.strptime(parsed, "%d.%m.%Y")
+            df_dict[schema['parser_type']] = parsed
 
         # Return Output data as dataframe for further use in mongoDB
-        return df_out
+        return df_dict
+
+    def parse_df_field(self, df: pd.DataFrame,
+                       search_column: str, search_str: str, target_column: str = None,
+                       split: str = None, output_float: bool = False):
+        if target_column is None:
+            target_column = search_column
+        parsed_field = df[target_column][df[search_column].str.contains(search_str, regex=True, na=False)].to_list()[0]
+        if split is not None:
+            parsed_field = parsed_field.split(split)[-1]
+        if output_float:
+            parsed_field = self.parse_float(parsed_field)
+        return parsed_field
+
+    def parse_float(self, number_str: str):
+        del_chars = ["-", " "]
+        for del_char in del_chars:
+            if del_char in number_str:
+                number_str = number_str.replace(del_char, "")
+
+        # Change german numbers to international ones
+        if "," in number_str:
+            number_str = number_str.replace(".", "")
+            number_str = number_str.replace(",", ".")
+        return float(number_str)
